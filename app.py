f977037bb6c6b9249b97852b0791596819630d76
kenneth1020 <trankenneth@sbcglobal.net>
2022-12-05 20:55:21 -0800

    Better TFT interfaces
    ITS ALIVE

--- app.py
+++ app.py
@@ -1,61 +1,58 @@
 #Library
+from django.http import HttpResponse
+from django.template import loader
 from flask import Flask, request, url_for, render_template
-from flask_navigation import Navigation
 from click import confirm
 from statement import openConnection, closeConnection, championSearch, championData
 import pandas as pd
 
 app = Flask(__name__)
-nav = Navigation(app)
 
-nav.Bar('top', [
-    nav.Item('Champion Items', 'championItems'),
-    nav.Item('Champion', 'champion'),
-])
+def testing(request):
+    template = loader.get_template('navbar.html')
+    return HttpResponse(template.render(request))
 
 @app.route('/',methods=['GET','POST'])
 def index():
     return render_template('index.html')
 
-@app.route('/championItems',methods =['GET','POST'])
-def championItems():
-    if request.method =='GET':
-            #this only takes in one input 'PrimaryEmail'
-            return '<form action="/" method="POST"><input name="Champion"><input type ="submit"></form>'  
-    database = r"tft_data.sqlite"
-    # create a database connection
-    conn = openConnection(database)
-    with conn:
-        search1 = request.form['Champion']
-        rows = championSearch(conn, search1)
-        print(rows)
+@app.route('/CISearch',methods =['GET','POST'])
+def CISearch():
+    if request.method == "POST":
+        search1 = request.form['cn']
 
+        database = r"tft_data.sqlite"
+        # create a database connection
+        conn = openConnection(database)
+        with conn:
+            rows = championSearch(conn, search1)
+            print(rows)
+
+            # creating dataframe
+            df = pd.DataFrame()
+            for row in rows:
+                df2 = pd.DataFrame(list(row)).T
+                df = pd.concat([df,df2])
     
-    # creating dataframe
-        df = pd.DataFrame()
-        for row in rows:
-            df2 = pd.DataFrame(list(row)).T
-            df = pd.concat([df,df2])
+            df.to_html('templates/ChampionItems.html')
+        
+        
+        return render_template('ChampionItems.html')
+
+    return render_template('CISearch.html')
     
-        df.to_html('templates/ChampionItems.html')
-   
-    return render_template('ChampionItems.html')
 
 
 @app.route('/champion')
-def champion():
-    if request.method =='GET':
-            #this only takes in one input 'PrimaryEmail'
-            return '<form action="/" method="POST"><input name="Champion"><input type ="submit"></form>'  
+def champion():     
     database = r"tft_data.sqlite"
     # create a database connection
     conn = openConnection(database)
     with conn:
-        search1 = request.form['Champion']
         rows = championData(conn)
         print(rows)
     
-     # creating dataframe
+    # creating dataframe
         df = pd.DataFrame()
         for row in rows:
             df2 = pd.DataFrame(list(row)).T
@@ -64,6 +61,7 @@ def champion():
         df.to_html('templates/ChampionData.html')
    
     return render_template('ChampionData.html')
+
 
 #Run the app
 if __name__ == '__main__':
